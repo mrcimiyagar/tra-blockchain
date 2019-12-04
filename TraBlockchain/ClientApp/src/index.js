273862d4fc53b7b3b3153c0da6af45b503ced5bc
Keyhan <theprogrammermachine@gmail.com>
2019-12-04 09:41:07 +0330

    fixing url

--- TraBlockchain/ClientApp/src/index.js
+++ TraBlockchain/ClientApp/src/index.js
@@ -5,10 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename="blockchain">
+  <BrowserRouter basename={baseUrl}>
     <App />
   </BrowserRouter>,
   rootElement);

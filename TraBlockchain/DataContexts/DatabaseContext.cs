6a6fb763780a37d87fddded2597e87d699138059
Pouya Pournasir <33330581+PouyaJigsaw@users.noreply.github.com>
2019-12-10 11:48:37 +0330

    Initial DbContext and entities

---
+++ TraBlockchain/DataContexts/DatabaseContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TraBlockchain
+{
+    public class DatabaseContext: DbContext
+    {
+        
+    }
+}
---
+++ TraBlockchain/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace TraBlockchain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Email { get; set; }
+    }
+}

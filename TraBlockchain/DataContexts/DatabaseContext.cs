28e81268756e2e2be22b8f6c8185e3052c9fdb2c
Pouya Pournasir <33330581+PouyaJigsaw@users.noreply.github.com>
2019-12-10 13:17:31 +0330

    Connect app to database

--- TraBlockchain/DataContexts/DatabaseContext.cs
+++ TraBlockchain/DataContexts/DatabaseContext.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using TraBlockchain.Entities;
 
 namespace TraBlockchain
 {
     public class DatabaseContext: DbContext
     {
+        private DbSet<User> Users { get; set; }
         
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=TraBlockchain;Username=postgres;Password=pouya258");
+
+
     }
 }

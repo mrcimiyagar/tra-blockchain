029460a0dbd6bd3c92bf10a306b10b03dfae4fe8
Pouya Pournasir <33330581+PouyaJigsaw@users.noreply.github.com>
2019-12-11 16:58:41 +0330

    seed some data

--- TraBlockchain/DataContexts/DatabaseContext.cs
+++ TraBlockchain/DataContexts/DatabaseContext.cs
@@ -9,8 +9,18 @@ namespace TraBlockchain
         
         // I FUCKIN DID IT!
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql("Host=localhost:5432;Database=TraBlockchain;Username=postgres;Password=pouya258");
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=TraBlockchain;Username=postgres;Password=pouya258");
 
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>()
+                .HasData(
+                    new User {Email = "pouya1pournasir@gmail.com", Id = 1, Name = "PouyaAdmin"},
+                    new User {Email = "Mohammad.zanjanchi@gmail.com", Id = 2, Name = "MohammadAdmin"}
+                );
+
+        }
     }
 }
---
+++ TraBlockchain/Migrations/20191211125730_SeedUsers.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TraBlockchain.Migrations
+{
+    public partial class SeedUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name" },
+                new object[,]
+                {
+                    { 1, "pouya1pournasir@gmail.com", "PouyaAdmin" },
+                    { 2, "Mohammad.zanjanchi@gmail.com", "MohammadAdmin" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}

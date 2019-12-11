a5639aa9f54d12c3217ff186131ddd53c3987cd7
Pouya Pournasir <33330581+PouyaJigsaw@users.noreply.github.com>
2019-12-11 17:19:54 +0330

    Added user controller

---
+++ TraBlockchain/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraBlockchain.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController: ControllerBase
+    {
+        
+    }
+}

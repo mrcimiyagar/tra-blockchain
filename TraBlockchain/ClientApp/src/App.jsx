99edfea356df30e1df906a8538fa711e258261e8
Pouya Pournasir <33330581+PouyaJigsaw@users.noreply.github.com>
2020-01-15 16:35:20 +0330

    Installed Axios

--- TraBlockchain/ClientApp/src/App.jsx
+++ TraBlockchain/ClientApp/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.scss";
 import { Login, Register } from "./components/login/index";
+import Axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
--- TraBlockchain/ClientApp/src/components/login/register.jsx
+++ TraBlockchain/ClientApp/src/components/login/register.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import loginImg from "../../login.svg";
 
 export class Register extends React.Component {
+  
   constructor(props) {
     super(props);
   }
